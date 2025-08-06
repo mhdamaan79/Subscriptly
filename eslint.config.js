ea1962c83eb0647be1a9f14327236f7281b4381c
Mohd Amaan <mhdamaan790@gmail.com>
2025-08-06 22:11:00 +0530

    setup config and routes

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,15 @@ import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        console: "readonly",
+        process: "readonly",
+      },
+    },
+  },
 ]);
