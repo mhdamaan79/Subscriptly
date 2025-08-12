3ffca9f9c94621e784141beef786df837b52aa06
Mohd Amaan <mhdamaan790@gmail.com>
2025-08-12 17:17:02 +0530

    started working on subscription controller

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,9 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
@@ -10,9 +15,7 @@ subscriptionRouter.get("/:id", (req, res) => {
   res.send({ title: "GET subscriptions details" });
 });
 
-subscriptionRouter.post("/", (req, res) => {
-  res.send({ title: "CREATE subscriptions" });
-});
+subscriptionRouter.post("/", authorize, createSubscription);
 
 subscriptionRouter.put("/:id", (req, res) => {
   res.send({ title: "UPDATE subscriptions" });
@@ -22,9 +25,7 @@ subscriptionRouter.delete("/:id", (req, res) => {
   res.send({ title: "DELETE subscriptions" });
 });
 
-subscriptionRouter.get("/user/:id", (req, res) => {
-  res.send({ title: "GET user subscriptions" });
-});
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
 
 subscriptionRouter.put("/:id/cancel", (req, res) => {
   res.send({ title: "CANCEL subscriptions" });
