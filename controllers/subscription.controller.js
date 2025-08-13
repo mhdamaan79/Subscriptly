31739c869a333a85ba8d02f6dac0801b1450cdb6
Mohd Amaan <mhdamaan790@gmail.com>
2025-08-13 14:27:29 +0530

    integrated upstash(reminder workflow) and nodemailer(send emails)

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,3 +1,5 @@
+import { SERVER_URL } from "../config/env.js";
+import { workflowClient } from "../config/upstash.js";
 import Subscription from "../models/subscription.model.js";
 
 export const getAllSubscriptions = async (req, res, next) => {
@@ -25,11 +27,22 @@ export const createSubscription = async (req, res, next) => {
       user: req.user._id,
     });
 
-    res.status(201).json({ success: true, data: subscription });
-  } catch (error) {
-    next(error);
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        'content-type': 'application/json',
+      },
+      retries: 0,
+    })
+
+    res.status(201).json({ success: true, data: { subscription, workflowRunId } });
+  } catch (e) {
+    next(e);
   }
-};
+}
 
 export const updateSubscriptions = async (req, res, next) => {
   try {
